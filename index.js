9ebd05e325121991f7d04c53e8e158e4d7b57fec
ismail <ismail.syedy@gmail.com>
2023-03-02 19:51:42 -0700

    done

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+    return  a + b;
+
+}
+function subtract(a,b){
+    return  a - b;
+
+}
+function multiply(a,b){
+    return  a * b;
+
+}
+function divide(a,b){
+    return  a / b;
+
+}
+function increment(a){
+    let n =1;
+    
+    return a + n;
+}
+function decrement(a){
+    let n =1;
+    
+    return a - n;
+}
+function makeInt(n){
+    parseInt('0x2328');
+    return parseInt(n,10);
+
+}
+function preserveDecimal(n){
+    
+    
+    parseFloat(2.222);
+    return parseFloat(n);
+}
